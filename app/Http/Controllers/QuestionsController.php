51052983c1b98cede1564e54c3b03a1de9f0a2eb
sergio <sergiotupaz@gmail.com>
2019-10-17 11:15:03 +0800

    Add author info. and question creation date in question item

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -13,10 +13,11 @@ class QuestionsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        $questions = Question::latest()->paginate(5);
+    {   
+        $questions = Question::with('user')->latest()->paginate(5);
+
+         return view('questions.index', compact('questions'));
         
-        return view('questions.index', compact('questions'));
     }
 
     /**
